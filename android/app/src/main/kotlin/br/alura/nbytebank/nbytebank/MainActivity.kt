74c50ce67a0f3a9a58f9d1d3662e683fb618a769
Heliomar P. Marques <heliomarpm@gmail.com>
2024-03-03 23:00:17 -0300

    update sdk version

--- android/app/src/main/kotlin/br/alura/nbytebank/nbytebank/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.alura.nbytebank
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/dev/heliomarpm/nbytebank/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.heliomarpm.nbytebank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
